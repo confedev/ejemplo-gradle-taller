cf9c1b8d23b6eff97578e340404eab2b9e8b6014
Felipe Contreras <confedev@gmail.com>
2022-01-29 18:08:52 -0300

    add gradle

---
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -0,0 +1,20 @@
+package com.devopsusach2020;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsUsach2020ApplicationTests {
+    @Test
+    void contextLoads() {
+        String correcto = "hola";
+        Assertions.assertThat(correcto).isEqualTo("hola");
+    }
+
+    @Test
+    void contextLoads2() {
+        String correcto = "hola";
+        Assertions.assertThat(correcto).isEqualTo("hola");
+    }
+}
